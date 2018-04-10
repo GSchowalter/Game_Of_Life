01db588de245e4b40ca951e3f37d3b9bc81ac608
Joshua Peck <peckjj@dukes.jmu.edu>
2018-04-09 22:35:49 -0400

    fix

--- Game_Of_Life/src/GameOfLife.java
+++ Game_Of_Life/src/GameOfLife.java
@@ -4,7 +4,7 @@ public class GameOfLife {
 	public static int screenWidth = 2000;
 	public static int screenHeight = 2000;
 	public static int length = 20;
-	public static double colonyWidth = (double) screenWidth / (double) Math.pow(length, 2);
+	public static double colonyWidth = (double) screenWidth / length;
 	
 	public static void main(String[] args)
 	{
