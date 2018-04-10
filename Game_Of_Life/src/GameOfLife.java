0cbd149736356f20c5612f9c7f0903568cf6d403
gschowalter <gschowalter@10.252.46.208>
2018-04-09 22:14:19 -0400

    ic

--- Game_Of_Life/src/GameOfLife.java
+++ Game_Of_Life/src/GameOfLife.java
@@ -3,6 +3,6 @@ public class GameOfLife {
 
 	public static void main(String[] args)
 	{
-		System.out.println("yo");
+		System.out.println("Hey Josh. When you see this I sant you to give me anothwr geeb. \nJust so you can indicate that you saw it \n that is all \n salude");
 	}
 }
