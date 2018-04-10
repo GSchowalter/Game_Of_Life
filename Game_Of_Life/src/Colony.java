e078704369baf210e510a2ecce5425a30c6059c4
Joshua Peck <peckjj@dukes.jmu.edu>
2018-04-09 22:44:01 -0400

    finished show() in Colony

--- Game_Of_Life/src/Colony.java
+++ Game_Of_Life/src/Colony.java
@@ -2,7 +2,7 @@
 public class Colony {
 
 	private boolean alive;
-	
+	// x and y on coordinate-plane, bottom left is (0,0)
 	private int x;
 	private int y;
 	
@@ -15,6 +15,14 @@ public class Colony {
 	
 	public void show(double length)
 	{
-		StdDraw.filledSquare(length/2, length/2, length/2);
+		if (alive)
+		{
+			StdDraw.setPenColor(StdDraw.YELLOW);
+		}
+		else
+		{
+			StdDraw.setPenColor(StdDraw.RED);
+		}
+		StdDraw.filledSquare((x * length) + length / 2, (y * length) + length / 2, length/2);
 	}
 }
