03d695f3fb8e45b6bb044666571c54a97b404491
gschowalter <gschowalter@10.252.46.208>
2018-04-10 16:01:00 -0400

    'WORKING LOGIC RULES without borders working'

--- Game_Of_Life/src/GameOfLife.java
+++ Game_Of_Life/src/GameOfLife.java
@@ -50,7 +50,6 @@ public class GameOfLife {
 			for (Colony curCol : curRow) {
 				if (curCol.getShouldTurnFate()) {
 					curCol.setFate();
-					curCol.setShouldTurnFate();
 				}
 			}
 		}
@@ -107,9 +106,12 @@ public class GameOfLife {
 
 	/**
 	 * Any live cell with fewer than two live neighbors dies, as if caused by under
-	 * population. Any live cell with two or three live neighbors lives on to the
-	 * next generation. Any live cell with more than three live neighbors dies, as
-	 * if by over population. Any dead cell with exactly three live neighbors
+	 * population. 
+	 * Any live cell with two or three live neighbors lives on to the
+	 * next generation. 
+	 * Any live cell with more than three live neighbors dies, as
+	 * if by over population. 
+	 * Any dead cell with exactly three live neighbors
 	 * becomes a live cell, as if by reproduction.
 	 * 
 	 * @param colony
