cf4975766ace328fa897110865ddc08fda611c4c
Peck <peckjj@l14359.cs.jmu.edu>
2018-04-11 13:51:34 -0400

    small sacling change

--- Game_Of_Life/src/GameOfLife.java
+++ Game_Of_Life/src/GameOfLife.java
@@ -8,8 +8,8 @@ public class GameOfLife {
 
 	public static final int DRAW_DELAY = 1000;
 
-	public static int buttonWidth = 100;
 	public static int screenHeight = 400;
+	public static int buttonWidth = screenHeight / 4;
 	public static int screenWidth = screenHeight + buttonWidth;
 	public static int buttonHeight = screenHeight / 5;
 	public static int length = 100;
